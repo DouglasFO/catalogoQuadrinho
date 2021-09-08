3c2a2370e5756c337e3511fe4191f898364a1bc0
DouglasFO <douglasfranca2290@gmail.com>
2021-09-08 08:12:35 -0300

    Criação do projeto

---
+++ src/main/java/br/edu/iff/catalogoQuadrinho/CatalogoQuadrinhoApplication.java
@@ -0,0 +1,13 @@
+package br.edu.iff.catalogoQuadrinho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogoQuadrinhoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogoQuadrinhoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/iff/catalogoQuadrinho/CatalogoQuadrinhoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.iff.catalogoQuadrinho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogoQuadrinhoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
